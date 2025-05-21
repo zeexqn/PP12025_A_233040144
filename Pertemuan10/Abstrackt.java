eef4705b4ec072f082e3abd99a94c543965c1678
Zeina <145121493+zeexqn@users.noreply.github.com>
2025-05-21 13:55:47 +0700

    Delete Pertemuan10 directory

--- Pertemuan10/Abstrackt.java
+++
@@ -1,9 +0,0 @@
-package Pertemuan10;
-
-
-	abstract class AbstractStack {
-	    // Abstract method to be implemented by subclasses
-	    abstract void displayStack(String s);
-	    abstract int peekN(int n);
-	}
-
